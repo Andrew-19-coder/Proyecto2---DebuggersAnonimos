0b0f8a0fb906bdcb2dc13df8fb1491ba296e3187
Jeshuan29 <jetorresgo@est.utn.ac.cr>
2025-08-23 17:48:11 -0600

    Clase Cliente
    Se realizan cambios en clientes, en el metodo licenciaVigente, tieneLicencia y se agrega el metodo setFechaExpedicion

--- src/Gestion_Personas/Clientes.java
+++ src/Gestion_Personas/Clientes.java
@@ -33,8 +33,12 @@ private LocalDate fechaExpedicion;
         return fechaExpedicion;
     }
     private boolean licenciaVigente(){
-        LocalDate hoy = LocalDate.now();
+         LocalDate hoy = LocalDate.now();
+        if(this.fechaExpedicion.isAfter(hoy)){
+            return false;
+        }
         return fechaExpedicion.plusYears(6).isAfter(hoy);
+       
     }
     public boolean tieneLicencia(){
         if(licenciaConducir==null){
@@ -43,9 +47,18 @@ private LocalDate fechaExpedicion;
         if(!licenciaConducir.equals(cedula)){
             return false;
         }
+        LocalDate fechaMayorEdad = this.fechaNacimiento.plusYears(18);
+        if(this.fechaExpedicion.isBefore(fechaMayorEdad)){
+           return false;
+        }
         if(!licenciaVigente()){
             return false;
         }
+        
         return true;
     } 
+       
+     public void setFechaExpedicion(LocalDate fechaExpedicion) {
+        this.fechaExpedicion = fechaExpedicion;
+    }
 }
